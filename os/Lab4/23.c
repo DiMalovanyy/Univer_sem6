3476d8005d1f6ffd5e13909ef68a63a4e3b2eafe
DiMalovanyy <dmitrymalovanyy@gmail.com>
2022-06-20 10:25:05 +0300

    small fix

--- os/Lab4/23.c
+++ os/Lab4/23.c
@@ -11,14 +11,14 @@
 #define INCREMENT_SIZE 100000
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
-void atomic_counter(_Atomic int* val) {
+void atomic_counter(atomic_int* val) {
 	for(int i = 0; i < INCREMENT_SIZE; i++) {
-		++val;
+		++(*val);
 	}
 }
 
 void* atomic_worker(void* thread_data) {
-	_Atomic int* val = (_Atomic int*)thread_data;
+	atomic_int* val = (atomic_int*)thread_data;
 	atomic_counter(val);
 	return NULL;
 }
@@ -46,19 +46,25 @@ void* mutex_worker(void* thread_data) {
 int main(int argc, char** argv) {
 
 	time_t start_atomic = time(NULL);
-	_Atomic int* value_a = 0;
+	atomic_int value_a = 0;
 	pthread_t first_atomic_th, second_atomic_th;
-	pthread_create(&first_atomic_th, NULL, atomic_worker,(void*)value_a);
-	pthread_create(&second_atomic_th, NULL, atomic_worker,(void*)value_a);
+	pthread_create(&first_atomic_th, NULL, atomic_worker,(void*)&value_a);
+	pthread_create(&second_atomic_th, NULL, atomic_worker,(void*)&value_a);
+
+	pthread_join(first_atomic_th, NULL);
+	pthread_join(second_atomic_th, NULL);
 	time_t end_atomic = time(NULL);
-	printf("Took %f seconds on atomic evaluation\n", difftime(end_atomic, start_atomic));
+	printf("Took %f seconds on atomic evaluation. Result: %d\n", difftime(end_atomic, start_atomic), value_a);
 
 	time_t start_mutex = time(NULL);
 	int value_m = 0;
 	pthread_t first_mutext_th, second_mutex_th;
 	pthread_create(&first_mutext_th, NULL, mutex_worker, (void*)&value_m);
 	pthread_create(&second_mutex_th, NULL, mutex_worker, (void*)&value_m);
+
+	pthread_join(first_mutext_th, NULL);
+	pthread_join(second_mutex_th, NULL);
 	time_t end_mutex = time(NULL);
-	printf("Took %f seconds on mutex evaluation\n", difftime(end_mutex, start_mutex));
+	printf("Took %f seconds on mutex evaluation. Result: %d\n", difftime(end_mutex, start_mutex), value_m);
 	return 0;
 }
